ab00f3c16399e28f1fd8170a70c1a8b210cb391b
DudaParanhos <dudaparanhosmoscou@gmail.com>
2025-02-28 20:37:12 -0300

    Criação da entidade clienterepository

--- src/main/java/com/fiap/ecr/api/repository/ClienteRepository.java
+++ src/main/java/com/fiap/ecr/api/repository/ClienteRepository.java
@@ -1,2 +1,4 @@
-package com.fiap.ecr.api.repository;public interface ClienteRepository {
-}
+package com.fiap.ecr.api.repository;
+import com.fiap.ecr.api.model.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {}
